430e40bc97666286af355f02e13c588e36fd8eff
danilopejatovic1988bgd <danilopejatovic1@gmail.com>
2020-05-07 23:44:59 +0200

    form-app

---
+++ src/Form.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import useForm from "./useForm";
+import validate from "./validateLogin";
+
+
+const Form = () => {
+
+    const { handleChange, handleSubmit, values, errors } = useForm(submit,validate);
+
+    function submit() {
+        alert("Successfully :)))))");
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} noValidate>
+                <div className="box">
+                    <label className="label">Email:</label>
+                    <div>
+                        <input className={`${errors.email && "inputError"}`} type="email" name="email" values={values.email} onChange={handleChange} />
+                        {errors.email && <p className="error">{errors.email}</p>}
+                    </div>
+                </div>
+                <div className="box">
+                    <label className="label">Password:</label>
+                    <div>
+                        <input className={`${errors.password && "inputError"}`} type="password" name="password" values={values.password} onChange={handleChange} />
+                        {errors.password && <p className="error">{errors.password}</p>}
+                    </div>
+                </div>
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
+    )
+}
+
+export default Form;
+
